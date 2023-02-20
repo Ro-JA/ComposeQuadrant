6d39647e2c58ee884fbf6cbbd2832114b65a558a
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-02-21 00:05:22 +0900

    tomorrow

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
@@ -30,14 +36,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ComposeQuadrantApp() {
+
 }
+
+@Composable
+fun ComposeQuadrant() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth().fillMaxHeight(0.5f)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(all = 16.dp).fillMaxHeight().fillMaxWidth(0.5f)
+                .background(Color.Green),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+
+
+        ) {
+            Text(
+                text = stringResource(id = R.string.column_composable_first),
+                modifier = Modifier.padding(bottom = 16.dp),
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = stringResource(id = R.string.text_composable_second),
+                textAlign = TextAlign.Justify
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(all = 16.dp)
+                .fillMaxHeight()
+                .fillMaxWidth(0.5f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+
+
+            ) {
+            Text(
+                text = stringResource(id = R.string.image_composable_first),
+                modifier = Modifier.padding(bottom = 16.dp),
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = stringResource(id = R.string.image_composable_second),
+                textAlign = TextAlign.Justify
+            )
+        }
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        ComposeQuadrant()
     }
 }
